9d4d8f9e7e58a81826a82bdcfcd68b3efa3e303c
Xiao.ma <Xiao.ma>
2017-05-10 14:38:53 +0800

    添加CPMData层.

--- examples/cpp_classification/classification.cpp
+++ examples/cpp_classification/classification.cpp
@@ -7,7 +7,8 @@ using namespace caffe;
 
 int main(int argc, char** argv)
 {
-	
+	caffe::Caffe::set_mode(caffe::Caffe::GPU);
+
 	caffe::Net<float> net("D:/dataset/COCO/training/experiments/test.prototxt",caffe::TRAIN);
 
 	net.Forward();
